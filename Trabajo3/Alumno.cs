cc361ab5146aa531f8a44618ea1fa64dc3fb68a1
Pilar Guisasola <TU_EMAIL_DE_GITHUB>
2026-09-20 12:44:52 -0300

    Etapa 1 clase Alumno

---
+++ Trabajo3/Alumno.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Alumno
+{
+    public string Nombre { get; set; }
+    public int Legajo { get; set; }
+    public double Nota1 { get; set; }
+    public double Nota2 { get; set; }
+}
--- Trabajo3/Program.cs
+++ Trabajo3/Program.cs
@@ -1,2 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
-// Practica Integradora 3
+﻿Alumno alumno1 = new Alumno();
+alumno1.Nombre = "Ana";
+alumno1.Legajo = 1234;
+alumno1.Nota1 = 7;
+alumno1.Nota2 = 8;
+
+Alumno alumno2 = new Alumno();
+alumno2.Nombre = "Juan";
+alumno2.Legajo = 5678;
+alumno2.Nota1 = 6;
+alumno2.Nota2 = 9;
+
+Console.WriteLine(alumno1.Nombre + " - " + alumno1.Legajo);
+Console.WriteLine(alumno2.Nombre + " - " + alumno2.Legajo);
+
+alumno1.Nombre = "Anita";
+
+Console.WriteLine(alumno1.Nombre);
+Console.WriteLine(alumno2.Nombre); 
