47eaa8c166b0c0cbd16ae5aaf42b03a578c02769
Pilar Guisasola <TU_EMAIL_DE_GITHUB>
2026-09-20 13:29:30 -0300

    Etapa 2 constructor

--- Trabajo3/Alumno.cs
+++ Trabajo3/Alumno.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-public class Alumno
+﻿public class Alumno
 {
     public string Nombre { get; set; }
     public int Legajo { get; set; }
     public double Nota1 { get; set; }
     public double Nota2 { get; set; }
+
+    public Alumno(string nombre, int legajo, double nota1, double nota2)
+    {
+        Nombre = nombre;
+        Legajo = legajo;
+        Nota1 = nota1;
+        Nota2 = nota2;
+    }
 }
+
+
+
+
--- Trabajo3/Program.cs
+++ Trabajo3/Program.cs
@@ -1,19 +1,6 @@
-﻿Alumno alumno1 = new Alumno();
-alumno1.Nombre = "Ana";
-alumno1.Legajo = 1234;
-alumno1.Nota1 = 7;
-alumno1.Nota2 = 8;
-
-Alumno alumno2 = new Alumno();
-alumno2.Nombre = "Juan";
-alumno2.Legajo = 5678;
-alumno2.Nota1 = 6;
-alumno2.Nota2 = 9;
+﻿Alumno alumno1 = new Alumno("Ana", 1234, 7, 8);
+Alumno alumno2 = new Alumno("Juan", 5678, 6, 9);
 
 Console.WriteLine(alumno1.Nombre + " - " + alumno1.Legajo);
 Console.WriteLine(alumno2.Nombre + " - " + alumno2.Legajo);
 
-alumno1.Nombre = "Anita";
-
-Console.WriteLine(alumno1.Nombre);
-Console.WriteLine(alumno2.Nombre); 
