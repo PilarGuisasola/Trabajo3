c13a08b897e2764ba3e59ec5b15f972bdffb7785
Pilar Guisasola <TU_EMAIL_DE_GITHUB>
2026-09-20 13:34:10 -0300

    Etapa 3 metodos

--- Trabajo3/Program.cs
+++ Trabajo3/Program.cs
@@ -1,6 +1,12 @@
 ﻿Alumno alumno1 = new Alumno("Ana", 1234, 7, 8);
-Alumno alumno2 = new Alumno("Juan", 5678, 6, 9);
+Alumno alumno2 = new Alumno("Juan", 5678, 4, 5);
 
-Console.WriteLine(alumno1.Nombre + " - " + alumno1.Legajo);
-Console.WriteLine(alumno2.Nombre + " - " + alumno2.Legajo);
+Console.WriteLine(alumno1.Nombre + " - Promedio: " + alumno1.Promedio());
+Console.WriteLine(alumno2.Nombre + " - Promedio: " + alumno2.Promedio());
 
+Console.WriteLine(alumno1.EstaAprobado());
+Console.WriteLine(alumno2.EstaAprobado());
+
+alumno2.SubirNota();
+
+Console.WriteLine("Nuevo promedio de Juan: " + alumno2.Promedio());
